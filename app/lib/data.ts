ef83838a668464d44000cfdd28c2ea322e3cb0de
christopherleavitt99 <cml95@byui.edu>
2024-10-02 20:15:27 -0600

    Chapter 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,6 +20,7 @@ export async function fetchRevenue() {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log("Data fetch completed after 3 seconds.");
+    console.log(invoice); // Invoice is an empty array []
 
     return data.rows;
   } catch (error) {
