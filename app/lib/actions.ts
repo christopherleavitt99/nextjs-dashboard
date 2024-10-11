68093e9b2cdded333c3d6f603b8def804d08b986
christopherleavitt99 <cml95@byui.edu>
2024-10-10 21:20:01 -0600

    minor updates

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,7 +68,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 export async function updateInvoice(
   id: string, 
-  prevState: State,
+  // prevState: State,
   formData: FormData) {
     const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
